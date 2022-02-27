2c144701eaa636df6ec3a4c646638ca9c4a3f75d
lambiv-dzenyuy <gillslambiv@gmail.com>
2022-02-27 10:05:39 +0100

    modifiction of payrol system done

---
+++ chapter10/ex10/fourteen/PieceWorker.java
@@ -0,0 +1,41 @@
+package chapter10.ex10.fourteen;
+
+public class PieceWorker extends Employee{
+    private double wage; // wage per piece sold
+    private int pieces; // number of pieces of sold
+
+    public PieceWorker(String firstName, String lastName, String socialSecurityNumber, double wage, int pieces) {
+        super(firstName, lastName, socialSecurityNumber);
+        this.wage = wage;
+        this.pieces = pieces;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+
+    public int getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(int pieces) {
+        this.pieces = pieces;
+    }
+
+    @Override
+    public double earnings() {
+        return wage*pieces;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("Piece employee: %s%n%s: $%,.2f; %s: %,2d",
+                super.toString(), "Wage per piece", getWage(),
+                "pieces sold", getPieces());
+    }
+}
